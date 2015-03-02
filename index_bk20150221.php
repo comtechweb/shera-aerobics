3dfc2a51dd4867d948e661091b3f7c325bea1ba3
Manjula Srinath <manjulawebx@gmail.com>
2015-03-02 12:21:46 +0530

    first commit. only index.html

---
+++ index_bk20150221.php
@@ -0,0 +1,273 @@
+<?php 
+include_once("includes/config.php");
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html lang="en" class="no-js ie6"><![endif]-->
+<!--[if IE 7 ]><html lang="en" class="no-js ie7"><![endif]-->
+<!--[if IE 8 ]><html lang="en" class="no-js ie8"><![endif]-->
+<!--[if IE 9 ]><html lang="en" class="no-js ie9"><![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><html lang="en" class="no-js"><!--<![endif]-->
+<head>
+    <meta charset="utf-8" />
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0" /> -->
+    <meta name="viewport" content="width=1024" />
+    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
+    <meta name="keywords" content="Ladies Fitness Center, Aerobics Classes in Sri Lanka, Aerobics Center Sri Lanka, Aerobics Center Gampaha, Aerobics Excersice" />
+    <meta name="description" content="Do you want to become slim? Choose the correct option.. Come and join with us.. Shera Aerobics - Ladies Fitness Center in Sri Lanka" />
+    <title>Shera Aerobics | Ladies Fitness Center | Aerobics Classes in Sri Lanka</title>
+    <link rel="shortcut icon" href="<?php echo SITE_ROOT; ?>images/favicon.ico" type="image/ico" />
+    <link rel="stylesheet" href="<?php echo SITE_ROOT; ?>css/foundation.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo SITE_ROOT; ?>css/flexslider.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="<?php echo SITE_ROOT; ?>css/animate.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo SITE_ROOT; ?>css/style.css" type="text/css" />
+    <script type="text/javascript" src="<?php echo SITE_ROOT; ?>js/vendor/modernizr.js"></script>
+    <!--[if lt IE 9]>
+        <script type="text/javascript" src="js/vendor/html5shiv.js"></script>
+        <script type="text/javascript" src="js/vendor/respond.js"></script>
+    <![endif]-->
+</head>
+<body class="loading">
+    <div id="container" class="clearfix">
+        <!--[if lt IE 9]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+        <header id="header" class="clearfix">
+            <div class="row">
+                <div id="logo_area" class="small-5 medium-5 columns">
+                    <a href="http://sheraaerobics.com/" title="www.sheraaerobics.com">
+                        <h1>Shera Aerobics</h1>
+                        <h2>Ladies Fitness Centre</h2>
+                    </a>
+                </div>
+                <div class="small-7 medium-7 columns">
+                    <nav class="navigaion">
+                        <ul>
+                            <li><a href="#about" title="About" class="nav_button">About</a></li>
+                            <li><a href="#instructor" title="Instructor" class="nav_button">Instructor</a></li>
+                            <li><a href="#classes" title="Classes" class="nav_button">Classes</a></li>
+                            <li><a href="#contact" title="Contact" class="nav_button">Contact</a></li>
+                        </ul>
+                    </nav>
+                    <a href="https://www.facebook.com/" target="_blank" title="Facebook" class="facebook">Facebook</a>
+                </div>
+                </div>
+               
+        </header>
+        <section id="scrollup"></section>
+        <div id="main_content" class="clearfix">
+            <section class="slider">
+                <div class="flexslider">
+                    <ul class="slides">
+                        <li>
+                            <img src="<?php echo SITE_ROOT; ?>images/aerobic-slide-01.jpg" alt="Ladies Aerobics Exercise | Fitness Center Sri Lanka" />
+                        </li>
+                        <li>
+                            <img src="<?php echo SITE_ROOT; ?>images/aerobic-slide-01.jpg" alt="Ladies Aerobics Exercise | Fitness Center Sri Lanka" />
+                        </li>
+                        <li>
+                            <img src="<?php echo SITE_ROOT; ?>images/aerobic-slide-01.jpg" alt="Ladies Aerobics Exercise | Fitness Center Sri Lanka" />
+                        </li>
+                        <li>
+                            <img src="<?php echo SITE_ROOT; ?>images/aerobic-slide-01.jpg" alt="Ladies Aerobics Exercise | Fitness Center Sri Lanka" />
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <section class="about content">
+                <div id="about_top">
+                    <div class="row">
+                        <div id="left" class="small-7 medium-7 columns">
+                            <div>
+                                <p>Young Looking</p><br/>
+                                <p>Elegant Figure</p><br/>
+                                <p>A slim, active shaped Body</p>
+                            </div>
+                            <img src="<?php echo SITE_ROOT; ?>images/about-aerobic-image-02.png" alt="About Aerobic Excercise" />
+                        </div>
+                        <div id="right" class="small-5 medium-5 columns">
+                            <img src="<?php echo SITE_ROOT; ?>images/about-aerobic-image-01.png" alt="About Ladies Fitness" />
+                        </div>
+                    </div>
+                </div>
+                <div id="about_bottom">
+                    <div class="row">
+                        <div class="small-12 medium-12 columns">
+                            <img src="<?php echo SITE_ROOT; ?>images/about-aerobic-image-03.png" alt="Aerobic for Healthy Life" >
+                            <div class="pwrap">
+                                <p class="animate">Stress Free</p><br/>
+                                <p class="animate">Burning of Fat</p><br/>
+                                <p class="animate">Balancing of Hormone</p><br/>
+                                <p class="animate">Process</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="instructor content">
+                <div class="row">
+                    <div class="small-12 columns">
+                        <h2>Instructor</h2>
+                    </div>
+                    <div class="small-5 medium-5 columns">
+                        <img src="<?php echo SITE_ROOT; ?>images/instructor-image-01.jpg" alt="Instructor | Ladies Fitess Instructor | Aerobic Instructor" class="animate">
+                    </div>
+                    <div class="small-7 medium-7 columns">
+                        <h3 class="animate">Mrs. Sheranthi Perera</h3>
+                        <h4 class="animate">Qualifications & Experience</h4>
+                        <p class="animate">Diploma Holder ( 1993) Ministry of Sports & Youth Affairs in Sri Lanka</p>
+                        <p class="animate">Sports Officer (North Western Province) - (1991-1994)</p>
+                        <p class="animate">Physical Training Instructor (Sri Lanka Air Force) - (1994 - 2009)</p>
+                        <p class="animate">Educations Teacher (Lyceum International School, Wattala) (2009 - 2013)</p>
+                    </div>
+                </div>
+            </section>
+            <section class="classes content">
+                <div class="row">
+                    <div class="small-12 columns">
+                        <h2>Classes</h2>
+                    </div>
+                    <div class="small-6 medium-6 columns">
+                        <div id="classtable_wrap" class="animate clearfix">
+                            <h5>Times & Dates of the Classes</h5>
+                            <p>(Please select 01 Class)</p>
+                            <table id="class_table" class="clearfix">
+                                <thead>
+                                    <tr>
+                                        <td class="clomain">Group</td>
+                                        <td>Monday</td>
+                                        <td>Tuesday</td>
+                                        <td>Thursday</td>
+                                        <td>Friday</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="clomain">A</td>
+                                        <td>09.00am<br/>10.15am</td>
+                                        <td></td>
+                                        <td>09.00am<br/>10.15am</td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="clomain">B</td>
+                                        <td></td>
+                                        <td>09.00am<br/>10.15am</td>
+                                        <td></td>
+                                        <td>09.00am<br/>10.15am</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="clomain">C</td>
+                                        <td>05.30am<br/>06.45am</td>
+                                        <td></td>
+                                        <td>05.30am<br/>06.45am</td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="clomain">D</td>
+                                        <td></td>
+                                        <td>05.30am<br/>06.45am</td>
+                                        <td></td>
+                                        <td>05.30am<br/>06.45am</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="clomain">E</td>
+                                        <td>07.15pm<br/>08.30pm</td>
+                                        <td></td>
+                                        <td>07.15pm<br/>08.30pm</td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="clomain">F</td>
+                                        <td></td>
+                                        <td>07.15pm<br/>08.30pm</td>
+                                        <td></td>
+                                        <td>07.15pm<br/>08.30pm</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div id="exclasses" class="small-6 medium-6 columns">
+                        <img src="<?php echo SITE_ROOT; ?>images/classes-image-01.jpg" alt="Aerobic Classes | Aerobic Class in Gampaha Sri Lanka" class="animate">
+                        <h2>Extra Classes</h2>
+                        <table id="exclass_table" class="animate">
+                            <tr>
+                                <td>Wednesday</td>
+                                <td><span>-</span>09.00am to 10.15am</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td><span></span>05.30pm to 06.45pm</td>
+                            </tr>
+                            <tr>
+                                <td>Saturday</td>
+                                <td><span>-</span>06.00pm to 07.15pm</td>
+                            </tr>
+                        </table>
+                        <p class="animate">(Freee of Charges & Everyone can participate)</p>
+                    </div>
+                </div>
+            </section>
+            <section class="contact content">
+                <div class="row">
+                    <div id="contact_details" class="small-6 columns">
+                        <h2>Contact</h2>
+                        <div class="small-6 columns" >
+                            <p class="animate">
+                                Mrs. Sheranthi Perera<br/>
+                                No. 335 C, Colombo Road,<br/>
+                                Gampaha
+                            </p>
+                            <p class="animate">
+                                Tel : 077-9268331
+                            </p>
+                        </div>
+                        <div class="small-6 columns">
+                            <P class="animate">Follow Us On :</P>
+                            <a href="https://www.facebook.com/" target="_blank" title="Follow Us On Facebook" ><span id="facebook_icon" class="animate"></span></a>
+                        </div>
+                        <div id="location" class="small-12 columns">
+                            <h2>Location</h2>
+                            <img src="<?php echo SITE_ROOT; ?>images/aerobic-class-location.jpg" alt="Aerobic Class Location Gampaha Sri lanka" class="animate">
+                        </div>
+                    </div> 
+                    <div class="small-6 columns">
+                        <h2>Get In Touch</h2>
+                        <div id="contact_form_wrap" >
+                            <form id="contact_form" class="mail_forms" name="contact_form" method="post" action="mailer/send-contact.php" novalidate>
+                                <div class="form_row animate" >
+                                    <input id="name" type="text" name="name" placeholder="Name" required />
+                                </div>
+                                <div class="form_row animate">
+                                    <input id="phone" name="phone" type="tel" placeholder="Phone" required />
+                                </div>
+                                <div class="form_row animate">
+                                    <input id="email" type="email" name="email" placeholder="Email" required />
+                                </div>
+                                <div class="form_row animate">  
+                                    <textarea id="message" rows="10" name="message" placeholder="Message" required></textarea>
+                                </div>
+                                <div class="form_row animate">  
+                                    <input type="hidden" value="<?php echo((isset($_SERVER["REMOTE_ADDR"])) ? $_SERVER["REMOTE_ADDR"] : "") ?>" name="ipaddress">
+                                    <input class="button small small-12" id="btn-submit" type="submit"  value="Submit" name="btn-submit">
+                                </div> 
+                            </form>
+                            <?php include_once(DOC_ROOT ."elements/form-msgs.php"); ?>
+                        </div>
+                    </div>     
+                </div>
+            </section>
+        </div>
+        <footer id="footer" class="clearfix">
+            <div class="row">
+                <div id="copyright">
+                    <p class="medium-6 columns ">&copy;2014. all rights reserved.</p>
+                    <p class="medium-6 columns text-right">website designed and developed by <a href="http://comtechlanka.com/" title="www.comtechlanka.com" target="_blank">Comtech</a></p>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <?php include_once(DOC_ROOT . "elements/doc-footer.php"); ?>
+</body>
+</html>
+
